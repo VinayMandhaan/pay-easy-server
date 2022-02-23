2e3b0a85298d0d296816e87c22883c3b73982d75
VinayMandhaan <vinaymandhaan29@gmail.com>
2022-02-24 03:05:07 +0500

    PayPal Server

---
+++ routes/api/paypal.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const router  = express.Router();
+const paypal = require("paypal-rest-sdk");
+
+paypal.configure({
+    mode: "sandbox", //sandbox or live
+    client_id:
+        "Af6f-lgva33MZiMBU8hcIzXZdWVjfC1_fHnDr53REFfS8c2mFvFr4sO6kKloboH0LQGhQJj1vwPzmDtq",
+    client_secret:
+        "ECDv8yqCEV0DWOPQaosZPrAmejmAnW9boC3TNf9n6MVjWjOx-7CDgBNMyJ6MCBQvv5Pn1LVvvEAkhDz5"
+  });
+
+
+  router.get('/payment/:price',(req, res) => {
+      console.log(req.params.price)
+    var create_payment_json = {
+        intent: "sale",
+        payer: {
+            payment_method: "paypal"
+        },
+        redirect_urls: {
+            return_url: `http://192.168.1.109:5000/api/paypal/success/${req.params.price}`,
+            cancel_url: "http://192.168.1.109:5000/api/paypal/cancel"
+        },
+        transactions: [
+            {
+                item_list: {
+                    items: [
+                        {
+                            name: "item",
+                            sku: "item",
+                            price: req.params.price,
+                            currency: "USD",
+                            quantity: 1
+                        }
+                    ]
+                },
+                amount: {
+                    currency: "USD",
+                    total: req.params.price
+                },
+                description: "This is the payment description."
+            }
+        ]
+    };
+
+    paypal.payment.create(create_payment_json, function(error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            console.log("Create Payment Response");
+            console.log(payment,'WORKEDDDDDDDDDDDDD');
+            res.redirect(`${payment.links[1].href}`);
+        }
+    });
+})
+
+
+
+router.get("/success/:total",async(req, res) => {
+  try {
+    var PayerID = req.query.PayerID;
+    var paymentId = req.query.paymentId;
+    
+    var execute_payment_json = {
+        payer_id: PayerID,
+        transactions: [
+            {
+                amount: {
+                    currency: "USD",
+                    total: req.params.total
+                }
+            }
+        ]
+    };
+
+    paypal.payment.execute(paymentId, execute_payment_json, function(
+        error,
+        payment
+    ) {
+        if (error) {
+            console.log(error.response);
+        } else {
+            console.log("Get Payment Response");
+            console.log(JSON.stringify(payment));
+            res.render("success");
+        }
+    });
+    // res.render("success");
+    // return res.status(201).send({ message: 'Payment Done Successfully', payment:paymentLog });
+  } catch (error) {
+    return res.json({"error":error})
+  }
+  // res.send("Success");
+
+});
+
+
+  
+module.exports=  router
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const path  = require('path')
+var cors = require('cors');
+const engines = require("consolidate");
+
+
+app.engine('ejs', require('ejs').__express)
+app.set("views", path.join(__dirname,'templates'))
+app.set("view engine", "ejs");
+app.use(cors())
+app.options('*', cors())
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.json({
+    extended: false
+}))
+
+app.get('/',(req,res)=>{
+    res.send('API Running')
+})
+
+app.use('/api/paypal',require('./routes/api/paypal'))
+
+app.listen(PORT, ()=>{
+    console.log(`Server Started on Port ${PORT}`)
+})
